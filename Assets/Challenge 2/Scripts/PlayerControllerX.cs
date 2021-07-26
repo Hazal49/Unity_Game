3dd9e24af110f96aed6ad3c42018284149fd275f
Hazal49 <yarimdunyahazal.31@gmail.com>
2021-07-26 14:34:56 +0300

    Unity_Game
    Destroy objects with collision

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float timestamp;
+    public float timeBetweenShots = 0.3333f;  // Saniyede 3 adet
+
+    public void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        // On spacebar press, send dog
+        if (Time.time >= timestamp && Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            timestamp = Time.time + timeBetweenShots;
+
+        }
+    }
+}
